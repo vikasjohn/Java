741eee5caae91607a30163fda13f805091a53168
vikasjohn <vikas.john@outlook.com>
2017-01-30 16:44:17 +0000

    second commit

--- src/practical1/QuestionThree.java
+++ src/practical1/QuestionThree.java
@@ -5,7 +5,15 @@ public class QuestionThree {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int num;
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the Number: ");
+		
+		num = input.nextInt();
+		total = num%10 + (num/10)num%100
+		                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+	
 	}
 
 }
